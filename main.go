ff99a794c52e7642b17caa88e81f1d0eff69be43
todd-estes <elestes@gmail.com>
2023-07-24 20:28:34 -0400

    Add /metrics endpoint html functionality

--- main.go
+++ main.go
@@ -26,9 +26,17 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+	w.Header().Add("Content-Type", "text/html")
+	w.Write([]byte(fmt.Sprintf(`
+	<html>
+
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
+
+  </html>
+	`, cfg.fileserverHits)))
 }
 
 func main() {
@@ -41,11 +49,14 @@ func main() {
 	// mux := http.NewServeMux()
 	mux := chi.NewRouter()
 	apir := chi.NewRouter()
+	adminRouter := chi.NewRouter()
 	mux.Mount("/api", apir)
+	mux.Mount("/admin", adminRouter)
 	mux.Handle("/app/*", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.Handle("/app", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	apir.Get("/healthz", getHealth)
-	apir.Get("/metrics", cfg.handleMetrics)
+	// apir.Get("/metrics", cfg.handleMetrics)  SWITCH OUT METRICS ENDPOINT
+	adminRouter.Get("/metrics", cfg.handleMetrics)
 
 
 	corsMux := middlewareCors(mux)
